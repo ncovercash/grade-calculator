35d553271c53db584da70566986fb286458c048e
Noah Overcash <me@ncovercash.dev>
2022-01-16 02:06:22 -0600

    Add testing html file for Next

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Test title</title>
+      </Head>
+      <section>
+        <h1>Testing</h1>
+      </section>
+    </div>
+  );
+}
